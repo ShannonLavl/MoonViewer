5e31669e66785c0f44c74cac94376e6665ffc215
Shannon Lavelle <lavelles@susqu.edu>
2015-02-22 17:17:07 -0500

    Chapter 15

--- app/src/main/java/com/susqu/lavelle/moonviewer/MoonViewerFragment.java
+++ app/src/main/java/com/susqu/lavelle/moonviewer/MoonViewerFragment.java
@@ -18,6 +18,12 @@ public class MoonViewerFragment extends Fragment {
     private Button mStopButton;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_moon_viewer, container, false);
 
